5528ae08aaec06e351b75934888fa80c3f2b96ce
Orel <orelmazuz1@gmail.com>
2025-04-02 13:33:42 +0300

    עשינו את הקוד יותר יפה

--- FC_Server/Controllers/UserController.cs
+++ FC_Server/Controllers/UserController.cs
@@ -3,167 +3,166 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace FC_Server.Controllers
+namespace FC_Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
+    // POST api/<UserController>/Register
+    [HttpPost("Register")]
+    public IActionResult Register([FromBody] User user)
     {
-        // POST api/<UserController>/Register
-        [HttpPost("Register")]
-        public IActionResult Register([FromBody] FC_Server.Models.User user)
+        try
         {
-            try
-            {
-                var newUser = FC_Server.Models.User.Register(user.Username, user.PasswordHash, user.Email, user.PhoneNumber);
+            var newUser = FC_Server.Models.User.Register(user.Username, user.PasswordHash, user.Email, user.PhoneNumber);
 
-                if (newUser != null)
-                {
-                    return Ok(new
-                    {
-                        message = "Registration successful",
-                        user = newUser
-                    });
-                }
-                return BadRequest(new { message = "Registration failed" });
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("Email already exists"))
-                {
-                    return BadRequest(new { message = "Email already exists" });
-                }
-                if (ex.Message.Contains("Phone already exists"))
-                {
-                    return BadRequest(new { message = "Phone already exists" });
-                }
-                return BadRequest(new { message = "Registration failed" });
-            }
-        }
-
-        // PUT api/<UserController>/UpdateUser
-        [HttpPut("UpdateUser")]
-        public IActionResult UpdateUser([FromBody] FC_Server.Models.User user)
-        {
-            try
-            {
-                var updatedUser = FC_Server.Models.User.UpdateUser(user.UserId, user.Username, user.PasswordHash, user.Email, user.PhoneNumber);
-
-                if (updatedUser != null)
-                {
-                    return Ok(new
-                    {
-                        message = "Profile updated successfully",
-                        user = updatedUser
-                    });
-                }
-                return BadRequest(new { message = "Update failed" });
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("Email already exists"))
-                {
-                    return BadRequest(new { message = "Email already exists" });
-                }
-                if (ex.Message.Contains("Phone already exists"))
-                {
-                    return BadRequest(new { message = "Phone already exists" });
-                }
-                return BadRequest(new { message = "Update failed" });
-            }
-        }
-
-        //Post api/<UserController>/Login
-        [HttpPost("Login")]
-        public IActionResult Login([FromBody] User user)
-        {
-            try
-            {
-                var userLoging = FC_Server.Models.User.Login(user.Email, user.PasswordHash);
-
-                if (userLoging != null)
-                {
-                    return Ok(new
-                    {
-                        message = "Login successful",
-                        user = new
-                        {
-                            user_id = userLoging.UserId,
-                            username = userLoging.Username,
-                            email = userLoging.Email,
-                            phone_number = userLoging.PhoneNumber,
-                            is_active = userLoging.IsActive,
-                            is_provider = userLoging.IsProvider
-                        }
-                    });
-                }
-                return BadRequest(new { message = "Invalid email or password" });
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("Account is not active"))
-                {
-                    return BadRequest(new { message = "Account is not active" });
-                }
-                return BadRequest(new { message = "Login failed" });
-            }
-        }
-
-        // GET: api/<UsersController> getUser
-        [HttpGet("getUser")]
-        public IActionResult getUser(int user_id)
-        {
-            var userData = FC_Server.Models.User.getUser(user_id);
-
-            if (userData != null)
+            if (newUser != null)
             {
                 return Ok(new
                 {
-                    message = "User data trensfer successful",
+                    message = "Registration successful",
+                    user = newUser
+                });
+            }
+            return BadRequest(new { message = "Registration failed" });
+        }
+        catch (Exception ex)
+        {
+            if (ex.Message.Contains("Email already exists"))
+            {
+                return BadRequest(new { message = "Email already exists" });
+            }
+            if (ex.Message.Contains("Phone already exists"))
+            {
+                return BadRequest(new { message = "Phone already exists" });
+            }
+            return BadRequest(new { message = "Registration failed" });
+        }
+    }
+
+    // PUT api/<UserController>/UpdateUser
+    [HttpPut("UpdateUser")]
+    public IActionResult UpdateUser([FromBody] User user)
+    {
+        try
+        {
+            var updatedUser = FC_Server.Models.User.UpdateUser(user.UserId, user.Username, user.PasswordHash, user.Email, user.PhoneNumber);
+
+            if (updatedUser != null)
+            {
+                return Ok(new
+                {
+                    message = "Profile updated successfully",
+                    user = updatedUser
+                });
+            }
+            return BadRequest(new { message = "Update failed" });
+        }
+        catch (Exception ex)
+        {
+            if (ex.Message.Contains("Email already exists"))
+            {
+                return BadRequest(new { message = "Email already exists" });
+            }
+            if (ex.Message.Contains("Phone already exists"))
+            {
+                return BadRequest(new { message = "Phone already exists" });
+            }
+            return BadRequest(new { message = "Update failed" });
+        }
+    }
+
+    //Post api/<UserController>/Login
+    [HttpPost("Login")]
+    public IActionResult Login([FromBody] User user)
+    {
+        try
+        {
+            var userLoging = FC_Server.Models.User.Login(user.Email, user.PasswordHash);
+
+            if (userLoging != null)
+            {
+                return Ok(new
+                {
+                    message = "Login successful",
                     user = new
                     {
-                        user_id = userData.UserId,
-                        username = userData.Username,
-                        email = userData.Email,
-                        phone_number = userData.PhoneNumber,
-                        is_active = userData.IsActive,
-                        is_provider = userData.IsProvider
+                        user_id = userLoging.UserId,
+                        username = userLoging.Username,
+                        email = userLoging.Email,
+                        phone_number = userLoging.PhoneNumber,
+                        is_active = userLoging.IsActive,
+                        is_provider = userLoging.IsProvider
                     }
                 });
             }
-            return BadRequest(new { message = "Invalid ID" });
+            return BadRequest(new { message = "Invalid email or password" });
         }
-
-        // GET: api/<UserController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        catch (Exception ex)
         {
-            return new string[] { "value1", "value2" };
+            if (ex.Message.Contains("Account is not active"))
+            {
+                return BadRequest(new { message = "Account is not active" });
+            }
+            return BadRequest(new { message = "Login failed" });
         }
+    }
 
-        // GET api/<UserController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    // GET: api/<UsersController> getUser
+    [HttpGet("getUser")]
+    public IActionResult getUser(int user_id)
+    {
+        var userData = FC_Server.Models.User.getUser(user_id);
 
-        // POST api/<UserController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        if (userData != null)
         {
+            return Ok(new
+            {
+                message = "User data trensfer successful",
+                user = new
+                {
+                    user_id = userData.UserId,
+                    username = userData.Username,
+                    email = userData.Email,
+                    phone_number = userData.PhoneNumber,
+                    is_active = userData.IsActive,
+                    is_provider = userData.IsProvider
+                }
+            });
         }
+        return BadRequest(new { message = "Invalid ID" });
+    }
 
-        // PUT api/<UserController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+    // GET: api/<UserController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
 
-        // DELETE api/<UserController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // GET api/<UserController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<UserController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UserController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UserController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
