05ae056b91551e38b61cacc6c8ca2f382cae7498
sharonref <sharonag15@gmail.com>
2025-04-14 17:20:18 +0300

    UpdateKnownLocation

--- FC_Server/DAL/DBservices.cs
+++ FC_Server/DAL/DBservices.cs
@@ -374,16 +374,80 @@ public class DBservices
     //--------------------------------------------------------------------------------------------------
     // This method updates users preferences
     //--------------------------------------------------------------------------------------------------
-    public UserPreferences? UpdateUserPreferences(int preference_id, int user_id, string shelter_type, bool accessibility_needed, int num_default_people, bool pets_allowed, DateTime last_update)
+    //public UserPreferences? UpdateUserPreferences(int preference_id, int user_id, string shelter_type, bool accessibility_needed, int num_default_people, bool pets_allowed, DateTime last_update)
+    //{
+    //SqlConnection con;
+    //SqlCommand cmd;
+    //UserPreferences? userPreferences = null;
+
+    //public UserPreferences? UpdateUserPreferences(int preferencesId, string shelterType, int myProperty) { return null; }
+    //}
+
+    //--------------------------------------------------------------------------------------------------
+    // This method updates users Known location
+    //--------------------------------------------------------------------------------------------------
+    public KnownLocation? UpdateKnownLocation(int location_id, int user_id, float latitude, float longitude, float radius, string address, string location_name, string nickname, DateTime added_at)
     {
         SqlConnection con;
         SqlCommand cmd;
-        UserPreferences? userPreferences = null;
-
-        public UserPreferences? UpdateUserPreferences(int preferencesId, string shelterType, int myProperty) { return null; }
-
-
-
+        KnownLocation? knownLocation = null;
+        try
+        {
+            con = connect("myProjDB");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Database connection error: " + ex.Message);
+        }
+        Dictionary<string, object> paramDic = new Dictionary<string, object>();
+        paramDic.Add("@location_id", location_id);
+        paramDic.Add("@user_id", user_id);
+        paramDic.Add("@latitude", latitude);
+        paramDic.Add("@longitude", longitude);
+        paramDic.Add("@radius", radius);
+        paramDic.Add("@address", address);
+        paramDic.Add("@location_name", location_name);
+        paramDic.Add("@nickname", nickname);
+        paramDic.Add("@added_at", added_at);
+        
+        cmd = CreateCommandWithStoredProcedureGeneral("FC_SP_UpdateKnownLocation", con, paramDic);
+        try
+        {
+            using (SqlDataReader dr = cmd.ExecuteReader())
+            {
+                if (dr.Read())
+                {
+                    knownLocation = new KnownLocation
+                    {
+                        LocationId = Convert.ToInt32(dr["location_id"]),
+                        UserId = Convert.ToInt32(dr["user_id"]),
+                        Latitude = Convert.ToSingle(dr["latitude"]),
+                        Longitude = Convert.ToSingle(dr["longitude"]),
+                        Radius = Convert.ToSingle(dr["radius"]),
+                        Address = dr["address"].ToString() ?? "",
+                        LocationName = dr["location_name"].ToString() ?? "",
+                        Nickname = dr["nickname"].ToString() ?? "",
+                        AddedAt = Convert.ToDateTime(dr["added_at"])
+                    };
+                }
+            }
+            return knownLocation;
+        }
+        catch (SqlException ex)
+        {
+            if (ex.Message.Contains("Invalid ID"))
+            {
+                throw new Exception("Invalid ID");
+            }
+            throw new Exception("Update failed");
+        }
+        finally
+        {
+            if (con != null && con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
     }
 
 }
