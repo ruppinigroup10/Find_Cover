f4be374b5849dce9b5a9234f80553b004dbe7355
sharonref <sharonag15@gmail.com>
2025-06-28 13:15:28 +0300

    clean defaults

--- FC_Server/Controllers/AlertController.cs
+++ FC_Server/Controllers/AlertController.cs
@@ -18,36 +18,5 @@ namespace FC_Server.Controllers
             return Ok(new { message = $"{alerts.Count} alerts processed" });
         }
     
-        // GET: api/<AlertController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<AlertController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<AlertController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<AlertController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<AlertController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
